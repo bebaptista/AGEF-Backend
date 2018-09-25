98f4e5fa65ccd00566f0e9ce8f7fd38e8c09e734
bebaptista <bernardo.baptista@sga.pucminas.br>
2018-09-25 16:00:19 -0300

    commit inicial

---
+++ src/main/java/com/bebaptista/AGEFbackend/AgefBackendApplication.java
@@ -0,0 +1,12 @@
+package com.bebaptista.AGEFbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgefBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgefBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bebaptista/AGEFbackend/AgefBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bebaptista.AGEFbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgefBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
