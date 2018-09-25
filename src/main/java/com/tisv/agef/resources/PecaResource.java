dff18e9906314466d5675cb44295c942715673de
bebaptista <bernardo.baptista@sga.pucminas.br>
2018-09-25 20:30:32 -0300

    Adicionar, Remover e listar pecas

--- src/main/java/com/tisv/agef/resources/PecaResource.java
+++ src/main/java/com/tisv/agef/resources/PecaResource.java
@@ -3,8 +3,11 @@ package com.tisv.agef.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +16,30 @@ import com.tisv.agef.domain.Peca;
 import com.tisv.agef.services.PecaService;
 
 @RestController
-@RequestMapping(value="/pecas")
+@RequestMapping(value="/peca")
 public class PecaResource {
 	
 	@Autowired
 	private PecaService service;
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
-	public ResponseEntity<?> findAll() {
-		List<Peca> obj = service.findAll();
-		
-		return ResponseEntity.ok(obj);
+	public List<Peca> listarPecas() {
+		return service.listarPecas();
 	}
+	
+	@PostMapping(value="/adicionar")
+	public void adicionarPeca(@RequestBody Peca peca){
+		service.adicionarPeca(peca);
+	}
+	
+	@DeleteMapping(value="/remover/{id}")
+	public void removerPeca(@PathVariable Integer id){
+		service.removerPeca(id);
+	}
+	/*
+	@PutMapping(value="/editar/{id}")
+	public void editarPeca(@RequestBody Peca peca, @PathVariable Integer id){
+		service.editarPeca(id);
+	}*/
+	
 }
--- src/main/java/com/tisv/agef/services/PecaService.java
+++ src/main/java/com/tisv/agef/services/PecaService.java
@@ -1,22 +1,37 @@
 package com.tisv.agef.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tisv.agef.domain.Peca;
 import com.tisv.agef.repositories.PecaRepository;
+import com.tisv.agef.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PecaService {
-	
+
 	@Autowired
 	private PecaRepository repo;
 
-	public List<Peca> findAll(){
+	public List<Peca> listarPecas() {
 		List<Peca> objs = repo.findAll();
 		return objs;
 	}
 	
+	public void adicionarPeca(Peca peca){
+		repo.save(peca);
+	}
+
+	public void removerPeca(Integer id) {
+		repo.deleteById(id);
+	}
+	
+	/*public void editarPeca(Integer id) {
+		Optional<Peca> obj = repo.findById(id);
+		obj.
+	}*/
+
 }
