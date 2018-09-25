3b9123c0db82841a3f6ba5d25c94c81a4aa99a7d
bebaptista <bernardo.baptista@sga.pucminas.br>
2018-09-25 16:50:45 -0300

    Peca mapping

--- src/main/java/com/tisv/agef/resources/PecaResource.java
+++ src/main/java/com/tisv/agef/resources/PecaResource.java
@@ -1,5 +1,10 @@
 package com.tisv.agef.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/pecas")
 public class PecaResource {
 
 }
