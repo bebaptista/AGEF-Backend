c98d8f70f5b559874cc6222f7b30001b10830745
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-09-25 21:57:17 -0300

    configuração do cors adicionada

---
+++ src/main/java/com/tisv/agef/configurations/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.tisv.agef.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@SuppressWarnings("deprecation")
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
+
+	}
+
+}
