f8ea5ca69ca6be4df128d6dac5ab44cca14d3f7e
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-09-26 02:10:35 -0300

    adicionado tratamento de exceção

--- src/main/java/com/tisv/agef/resources/PecaResource.java
+++ src/main/java/com/tisv/agef/resources/PecaResource.java
@@ -2,45 +2,54 @@ package com.tisv.agef.resources;
 
 import java.util.List;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
 import com.tisv.agef.domain.Peca;
 import com.tisv.agef.services.PecaService;
 
 @RestController
-@RequestMapping(value="/peca")
+@RequestMapping(value = "/peca")
 public class PecaResource {
-	
+
 	@Autowired
 	private PecaService service;
-	
-	@RequestMapping(value="/listar", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public List<Peca> listarPecas() {
 		return service.listarPecas();
 	}
-	
-	@PostMapping(value="/adicionar")
-	public void adicionarPeca(@RequestBody Peca peca){
+
+	@PostMapping(value = "/adicionar")
+	public void adicionarPeca(@RequestBody Peca peca) {
 		service.adicionarPeca(peca);
 	}
-	
-	@DeleteMapping(value="/remover/{id}")
-	public void removerPeca(@PathVariable Integer id){
+
+	@DeleteMapping(value = "/remover/{id}")
+	public void removerPeca(@PathVariable Integer id) {
 		service.removerPeca(id);
 	}
-	
-	@PutMapping(value="/editar/{id}")
-	public ResponseEntity<?> editarPeca(@RequestBody Peca peca, @PathVariable Integer id){
+
+	@PutMapping(value = "/editar/{id}")
+	public ResponseEntity<?> editarPeca(@RequestBody Peca peca, @PathVariable Integer id) {
 		return service.editarPeca(peca, id);
 	}
-	
+
+	@ExceptionHandler(ConstraintViolationException.class)    
+	public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+		return new ResponseEntity<>("Existe alguma constraint na entidade sendo utilizada.", HttpStatus.BAD_REQUEST);
+	}
 }
--- src/main/java/com/tisv/agef/services/PecaFeiraService.java
+++ src/main/java/com/tisv/agef/services/PecaFeiraService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.tisv.agef.domain.Peca;
 import com.tisv.agef.domain.PecaFeira;
 import com.tisv.agef.repositories.PecaFeiraRepository;
 
